54216155b46df63170a9640edb53bb64efb2ea7e
Robert Takhvar <31373997+TimelessPWN@users.noreply.github.com>
2017-09-05 20:54:08 -0700

    Create knoxUninstall.sh
    First Draft

---
+++ knoxUninstall.sh
@@ -0,0 +1,71 @@
+pm clear com.sec.android.app.sysscope
+pm disable com.sec.android.app.sysscope
+pm uninstall --user 0 com.sec.android.app.sysscope
+usleep 5000
+pm clear com.samsung.android.bbc.bbcagent
+pm disable com.samsung.android.bbc.bbcagent
+pm uninstall --user 0 com.samsung.android.bbc.bbcagent
+usleep 5000
+pm clear com.samsung.android.bbc.fileprovider
+pm disable com.samsung.android.bbc.fileprovider
+pm uninstall --user 0 com.samsung.android.bbc.fileprovider
+usleep 5000
+pm clear com.samsung.knox.knoxtrustagent
+pm disable com.samsung.knox.knoxtrustagent
+pm uninstall --user 0 com.samsung.knox.knoxtrustagent
+usleep 5000
+pm clear com.samsung.knox.kss
+pm disable com.samsung.knox.kss
+pm uninstall --user 0 com.samsung.knox.kss
+usleep 5000
+pm clear com.sec.enterprise.knox.attestation
+pm disable com.sec.enterprise.knox.attestation
+pm uninstall --user 0 com.sec.enterprise.knox.attestation
+usleep 5000
+pm clear com.sec.enterprise.knox.shareddevice.keyguard
+pm disable com.sec.enterprise.knox.shareddevice.keyguard
+pm uninstall --user 0 com.sec.enterprise.knox.shareddevice.keyguard
+usleep 5000
+pm clear com.sec.knox.bluetooth
+pm disable com.sec.knox.bluetooth
+pm uninstall --user 0 com.sec.knox.bluetooth
+usleep 5000
+pm clear com.sec.knox.containeragent2
+pm disable com.sec.knox.containeragent2
+pm uninstall --user 0 com.sec.knox.containeragent2
+usleep 5000
+pm clear com.sec.knox.shortcutsms
+pm disable com.sec.knox.shortcutsms
+pm uninstall --user 0 com.sec.knox.shortcutsms
+usleep 5000
+pm clear com.sec.enterprise.mdm.services.simpin
+pm disable com.sec.enterprise.mdm.services.simpin
+pm uninstall --user 0 com.sec.mdm.services.simpin
+usleep 5000
+pm clear com.sec.enterprise.mdm.vpn
+pm disable com.sec.enterprise.mdm.vpn
+pm uninstall --user 0 com.sec.enterprise.mdm.vpn
+usleep 5000
+pm clear com.samsung.ucs.agent.ese
+pm disable com.samsung.ucs.agent.ese
+pm uninstall --user 0 com.samsung.ucs.agent.ese
+usleep 5000
+pm clear com.samsung.klmsagent
+pm disable com.samsung.klmsagent
+pm uninstall --user 0 com.samsung.klmsagent
+usleep 5000
+pm clear com.sec.knox.switcher
+pm disable com.sec.knox.switcher
+pm uninstall --user 0 com.sec.knox.switcher
+usleep 5000
+pm clear com.sec.enterprise.knox.cloudmdm.smdms
+pm disable com.sec.enterprise.knox.cloudmdm.smdms
+pm uninstall --user 0 com.sec.enterprise.knox.cloudmdm.smdms
+usleep 5000
+pm clear com.sec.knox.knoxsetupwizardclient
+pm disable com.sec.knox.knoxsetupwizardclient
+pm uninstall --user 0 com.sec.knox.knoxsetupwizardclient
+usleep 5000
+pm clear com.samsung.knox.appsupdateagent
+pm disable com.samsung.knox.appsupdateagent
+pm uninstall --user 0 com.samsung.knox.appsupdateagent
